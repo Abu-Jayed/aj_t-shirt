c8575ea8a8d83556fd68a47b8c17edc9cd25424f
Abu Jayed <aj.webdevlopment@gmail.com>
2023-04-07 10:23:40 +0600

    basic set up done

---
+++ src/components/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+    return (
+        <div>
+            <h2>This is what customar think about us.</h2>
+        </div>
+    );
+};
+
+export default OrderReview;
